5a9209b24b82cb02cd96b13e198ebc08dfd288ab
Vail <vac68@cornell.edu>
2024-03-27 01:49:04 -0400

    added user names and passwords

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,31 @@
-let () = print_endline "Hello, World!"
-let () =
-  print_endline ("What would you like to do?");
+
+(*Opens password prompt for Khoas's account, can be abstracted*)
+let khoa () = 
+  print_endline ("Please enter your password");
+  let user_input = read_line() in
+    if user_input = "ktn9" then print_endline("Password accepted, hello Khoa") else
+      print_endline("User or password is incorrect")
+      let () = print_endline "Hello, World!"
+(*Opens password prompt for Kyle's account*)
+let kyle () = 
+  print_endline ("Please enter your password");
+  let user_input = read_line() in
+    if user_input = "kgc42" then print_endline("Password accepted, hello Kyle") else
+      print_endline("User or password is incorrect")
+(*Opens password prompt for Vail's account*)
+let vail () = 
+  print_endline ("Please enter your password");
+  let user_input = read_line() in
+    if user_input = "vac68" then print_endline("Password accepted, hello Vail") else
+      print_endline("User or password is incorrect")
+(*Starts budget app*)
+let rec startup () =
+  print_endline ("Welcome to Pi r Squared budgeting! Please enter your name");
   let user_input = read_line () in 
-    print_endline(user_input)
+    if user_input = "khoa" then khoa ()
+    else if user_input = "kyle" then kyle ()
+    else if user_input = "vail" then vail ()
+    else 
+      print_endline ("User not recognized, please try again");
+      startup ()
+let () = startup ()
