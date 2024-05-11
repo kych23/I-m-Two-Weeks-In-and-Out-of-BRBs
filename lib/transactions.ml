9774dc63cae6b31deb0fbc4a5a9828930251f28e
Kyle Chu <kgc42@cornell.edu>
2024-05-10 22:13:45 -0400

    transactions new features

--- lib/transactions.ml
+++ lib/transactions.ml
@@ -5,4 +5,15 @@ type transaction = {
   category : string;
 }
 
+let ensure_directory_exists path =
+  if not (Sys.file_exists path) then Unix.mkdir path 0o755
+
+let create_and_save_csv_in_folder folder filename =
+  ensure_directory_exists folder;
+  let path = Filename.concat folder filename in
+  Csv.save path []
+
+let create_new_transaction_file username =
+  create_and_save_csv_in_folder "/data" username
+
 let create_transaction id date amount category = { id; date; amount; category }
--- lib/transactions.mli
+++ lib/transactions.mli
@@ -1,3 +1,4 @@
 type transaction
 
+val create_new_transaction_file : string -> unit
 val create_transaction : int -> string -> float -> string -> transaction
