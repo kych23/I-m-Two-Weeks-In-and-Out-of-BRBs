fbafc62ccaa5496da8311855e1be25a317abb5b3
Khoa Nguyen <ktn9@cornell.edu>
2024-05-10 22:15:04 -0400

    Implemented some more functions for transactions compilation unit

--- lib/transactions.ml
+++ lib/transactions.ml
@@ -6,3 +6,39 @@ type transaction = {
 }
 
 let create_transaction id date amount category = { id; date; amount; category }
+
+let load_transactions filename =
+  let table = Csv.load filename in
+  List.map
+    (function
+      | [ id; date; amount; category ] ->
+          {
+            id = int_of_string id;
+            date;
+            amount = float_of_string amount;
+            category;
+          }
+      | _ -> failwith "error with csv")
+    table
+
+let save_transactions filename transactions =
+  let data =
+    List.map
+      (fun txn ->
+        [
+          string_of_int txn.id;
+          txn.date;
+          string_of_float txn.amount;
+          txn.category;
+        ])
+      transactions
+  in
+  Csv.save filename data
+
+let add_transaction new_txn transactions =
+  if List.exists (fun txn -> txn.id = new_txn.id) transactions then
+    failwith "Transaction ID already exists"
+  else new_txn :: transactions
+
+let delete_transaction txn_id transactions =
+  List.filter (fun txn -> txn.id <> txn_id) transactions
--- lib/transactions.mli
+++ lib/transactions.mli
@@ -1,3 +1,16 @@
 type transaction
 
 val create_transaction : int -> string -> float -> string -> transaction
+(** [crate_transaction id date amount category] is a new [transaction] record
+    with [id] id on day [date] of the amount [amount] and category [category]. *)
+
+val load_transactions : string -> transaction list
+(** [load_transactions filename] outputs the transactions of the user's
+    transaction's file which has [filename]. *)
+
+val save_transactions : string -> transaction list -> unit
+(** [save_transactions filename transactions] saves the list of transactions
+    [transactions] to the file [filename]. *)
+
+val add_transaction : transaction -> transaction list -> transaction list
+val delete_transaction : transaction -> transaction list -> transaction list
